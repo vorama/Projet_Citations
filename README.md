ba3d4b426856a8fb92f545c3d96be3114c249f44
Amandine Vorpe <amandine.vorpe@he-arc.ch>
2025-03-25 16:11:32 +0100

    commit du 25.03 mais l'appli marche plus

---
+++ README.md
@@ -0,0 +1,20 @@
+# Citations - Application Web
+
+Voici mon application web qui fournit des ciations motivantes de manière aléatoire et sur demande
+
+Description:
+L'application fourni une interface web ou l'on peut recevoir des citations motivantes.
+elle est composée en deux parties.
+    1. le frontend: C'est une page web simple qui affiche une nouvelle citation lorsque l'on presse sur le bouton "nouvelle citation"
+    2. le backend: C'est une API Flask qui renvoie une citations parmis une liste de manière aléatoire
+
+Prérequis:
+Il vous faudra:
+
+    - [Docker](https://www.docker.com/) : Pour construire et exécuter des conteneurs.
+    - [Docker Compose](https://docs.docker.com/compose/) : Pour faciliter la gestion des services conteneurisés.
+
+
+
+
+# rbarazzutti
--- backend/citations.py
+++ backend/citations.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
-'''import la classe Flask et la fonction jsonify depuis le module flask
+'''
+import la classe Flask et la fonction jsonify depuis le module flask
 on pourait faire comme suite:
 from flask import Flask
 from flask import jsonify
@@ -10,28 +11,42 @@ import random
 
 
 app = Flask(__name__)
-''' flash back du cours de python:
+'''
+flash back du cours de python:
 Flask = la classe
 app = une instance
 __name__ = ma futur variable
 '''
 
 citations = [
-# "citations" une liste de dictionnaire. Chaque {} représente un dictionnaire avec, sur chaques lignes, 2 clés(quote + author) et deux valeurs 
+''' "citations" une liste de dictionnaire
+ Chaque {} représente:
+  - 1 dictionnaire par lignes (quote)
+  - 2 clés(quote + author) et deux valeurs
+'''
     {"quote": "Tout est possible à qui rêve, ose, travaill et n'abandonne jamais.", "author": "Xavier Dolan"},
     {"quote": "Ceux qui ne font rien de se trompent jamias", "author": "Théodore de Banvill"},
-    {"quote": "Vous ne pouvez pas être ce gamin qui reste figé en haut d'un toboggan en réfléchissant. Vous devez glisser", "author": "Tina Fey"},
-    
+    {"quote": "Vous ne pouvez pas être ce gamin qui reste figé en haut d'un toboggan en réfléchissant. Vous devez glisser", "author": "Tina Fey"},  
 ]
 
 
-# Route pour la racine (/), ajoutée ici
 @app.route('/')
+'''
+@app.route() = Décorateur qui associe un URL à la fonction python qui suie (on appelle ça une route)
+('/') = c'est pour dire que la route pointe sur le menu du site
+http://localhost:5000/ rien qui suie
+'''
 def home():
+#affiche la phrase
     return "Bienvenue sur l'API Citations!"
 
 
 @app.route('/quote', methods=['GET'])
+'''
+C'est un décorateur Flask
+@app.route('/qute' = décorateur même chose qu'en ligne 36
+methods=['GET]) = requête HTTP avec Flask
+'''
 def donner_une_citation ():
     return jsonify(random.choice(citations))
     # donner un citation random de la liste présédente
