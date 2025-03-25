1973a4e660b74622f9e6d1bad86c7d22b1db0a72
Raphaël P. Barazzutti <raphael@barazzutti.net>
2025-03-25 19:14:34 +0100

    fix: commentaires

--- backend/citations.py
+++ backend/citations.py
@@ -18,35 +18,35 @@ app = une instance
 __name__ = ma futur variable
 '''
 
-citations = [
 ''' "citations" une liste de dictionnaire
  Chaque {} représente:
   - 1 dictionnaire par lignes (quote)
   - 2 clés(quote + author) et deux valeurs
 '''
+citations = [
     {"quote": "Tout est possible à qui rêve, ose, travaill et n'abandonne jamais.", "author": "Xavier Dolan"},
     {"quote": "Ceux qui ne font rien de se trompent jamias", "author": "Théodore de Banvill"},
     {"quote": "Vous ne pouvez pas être ce gamin qui reste figé en haut d'un toboggan en réfléchissant. Vous devez glisser", "author": "Tina Fey"},  
 ]
 
 
-@app.route('/')
 '''
 @app.route() = Décorateur qui associe un URL à la fonction python qui suie (on appelle ça une route)
 ('/') = c'est pour dire que la route pointe sur le menu du site
 http://localhost:5000/ rien qui suie
 '''
+@app.route('/')
 def home():
-#affiche la phrase
+    #affiche la phrase
     return "Bienvenue sur l'API Citations!"
 
 
-@app.route('/quote', methods=['GET'])
 '''
 C'est un décorateur Flask
 @app.route('/qute' = décorateur même chose qu'en ligne 36
 methods=['GET]) = requête HTTP avec Flask
 '''
+@app.route('/quote', methods=['GET'])
 def donner_une_citation ():
     return jsonify(random.choice(citations))
     # donner un citation random de la liste présédente
